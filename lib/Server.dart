87ce55357e1c4097a0bd22bea2635b6fd2fae5f3
MarisabelC <38087342+MarisabelC@users.noreply.github.com>
2020-12-17 14:01:28 -0800

    update AndroidManifest.xml

--- lib/Server.dart
+++ lib/Server.dart
@@ -1,16 +1,45 @@
 import 'dart:async';
+import 'dart:io';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-class Server{
+class Server {
+  FirebaseFirestore _db;
+  final databaseReference = FirebaseDatabase();
+  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
 
-  final databaseReference = FirebaseDatabase.instance.reference();
+  Server() {
+    _init();
+  }
+  _init() async {
+    await Firebase.initializeApp().then((value) {
+      _db = FirebaseFirestore.instance;
+    });
+  }
 
   Future getTimestamps(String device) async {
-
-    DataSnapshot ds = await databaseReference.child(
-        "device/"+ device+"/timestamp").once();
+    DataSnapshot ds = await databaseReference
+        .reference()
+        .child("device/" + device + "/timestamp")
+        .once();
     print('********getting timestamps: ' + ds.value.toString());
     return ds.value;
   }
 
+  Future<String> saveDeviceToken() async {
+    await Future.delayed(Duration(seconds: 2));
+    String fcmToken = await _firebaseMessaging.getToken();
+
+    if (fcmToken != null) {
+      var tokenRef =
+          _db.collection('tokens').doc(fcmToken);
+      await tokenRef.set({
+        'token': fcmToken,
+        'createAt': FieldValue.serverTimestamp(),
+        'platform': Platform.operatingSystem
+      });
+    }
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,14 @@ import "RecordPage.dart";
 import 'package:flutter/material.dart';
 import 'package:splashscreen/splashscreen.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
-void main(){
+import 'dart:io' show Platform;
+import 'Server.dart';
+
+void main() {
   runApp(new MaterialApp(
     home: new MyApp(),
   ));
 }
-
 
 class MyApp extends StatefulWidget {
   @override
@@ -17,23 +19,34 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
   var _message = '';
+  Server server = Server();
+
   _register() {
-    _firebaseMessaging.getToken().then((token) => print(token));
+    if (Platform.isIOS) {
+      _firebaseMessaging.onIosSettingsRegistered.listen((event) {
+        server.saveDeviceToken();
+      });
+      _firebaseMessaging
+          .requestNotificationPermissions(IosNotificationSettings());
+    } else {
+      server.saveDeviceToken();
+    }
   }
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
+    _register();
     getMessage();
-  _register();}
+  }
 
-  void getMessage(){
+  void getMessage() {
     _firebaseMessaging.configure(
         onMessage: (Map<String, dynamic> message) async {
-          print('on message $message');
-          setState(() => _message = message["notification"]["title"]);
-        }, onResume: (Map<String, dynamic> message) async {
+      print('on message $message');
+      setState(() => _message = message["notification"]["title"]);
+    }, onResume: (Map<String, dynamic> message) async {
       print('on resume $message');
       setState(() => _message = message["notification"]["title"]);
     }, onLaunch: (Map<String, dynamic> message) async {
@@ -41,28 +54,27 @@ class _MyAppState extends State<MyApp> {
       setState(() => _message = message["notification"]["title"]);
     });
   }
+
   @override
   Widget build(BuildContext context) {
     return new SplashScreen(
       seconds: 5,
       navigateAfterSeconds: new AfterSplash(),
-      title: new Text('Welcome In SplashScreen',
-        style: new TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 20.0
-        ),
+      title: new Text(
+        'Welcome In SplashScreen',
+        style: new TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
       ),
       image: new Image.asset('asset/Shiba_Smile.png'),
       backgroundColor: Colors.white,
       styleTextUnderTheLoader: new TextStyle(),
       photoSize: 100.0,
-      onClick: ()=>print("Flutter Egypt"),
+      onClick: () => print("Flutter Egypt"),
       loaderColor: Colors.red,
     );
   }
 }
 
-class AfterSplash extends StatelessWidget{
+class AfterSplash extends StatelessWidget {
   var deviceController = TextEditingController();
   @override
   Widget build(BuildContext context) {
@@ -78,56 +90,68 @@ class AfterSplash extends StatelessWidget{
                 image: AssetImage('asset/background.png'), fit: BoxFit.cover),
           ),
           child: new Center(
-              child:  Column(
+              child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
-                    Padding (
-                        padding: EdgeInsets.only(left: 20, right: 20, top: 20),
-                        child: Row(
-                          children: [
-                            Expanded(flex:3,
-                              child: TextField(
-
-                                obscureText: false,
-                                style: TextStyle(fontSize: 30,fontFamily: 'Lato', color: Colors.brown),
-                                //style: ,
-                                controller: deviceController,
-                                decoration: InputDecoration(
-                                  prefixIcon: Icon(Icons.phone_android,color: Colors.brown,),
-                                  border: new OutlineInputBorder(borderSide: BorderSide(color: Colors.brown),
-                                    borderRadius: new BorderRadius.circular(30.0),
-                                  ),
-                                  focusedBorder:OutlineInputBorder(borderSide: BorderSide(color: Colors.brown),
-                                    borderRadius: new BorderRadius.circular(30.0),
-                                  ),
-                                  labelText: 'Device',
-                                  labelStyle: TextStyle(fontSize: 30, fontFamily: 'Lato', color: Colors.brown,
-                                  ),
-                                ),
+                Padding(
+                    padding: EdgeInsets.only(left: 20, right: 20, top: 20),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          flex: 3,
+                          child: TextField(
+                            obscureText: false,
+                            style: TextStyle(
+                              fontSize: 30,
+                              fontFamily: 'Lato',
+                              color: Colors.brown,
+                            ),
+                            //style: ,
+                            controller: deviceController,
+                            decoration: InputDecoration(
+                              filled: true,
+                              fillColor: Colors.white,
+                              prefixIcon: Icon(
+                                Icons.phone_android,
+                                color: Colors.brown,
+                              ),
+                              border: new OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.brown),
+                                borderRadius: new BorderRadius.circular(30.0),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.brown),
+                                borderRadius: new BorderRadius.circular(30.0),
+                              ),
+                              labelText: 'Device',
+                              labelStyle: TextStyle(
+                                fontSize: 30,
+                                fontFamily: 'Lato',
+                                color: Colors.brown,
                               ),
                             ),
-                            Expanded(
-                              flex: 1,
-                              child: IconButton(
-                                icon: Icon(Icons.forward),
-                                onPressed: () {
-                                  print(deviceController.text);
-                                  Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) => RecordPage(
-                                          title: deviceController.text,)),
-                                  );
-
-                                },
-                              ),
-                            )],
+                          ),
+                        ),
+                        Expanded(
+                          flex: 1,
+                          child: IconButton(
+                            icon: Icon(Icons.forward),
+                            onPressed: () {
+                              print(deviceController.text);
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => RecordPage(
+                                          title: deviceController.text,
+                                        )),
+                              );
+                            },
+                          ),
                         )
-                    ),
-                    SizedBox(height: 25.0),
-                  ])
-
-          ),)
-    );
+                      ],
+                    )),
+                SizedBox(height: 25.0),
+              ])),
+        ));
   }
 }
